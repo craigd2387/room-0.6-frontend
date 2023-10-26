93b25ec4d5e9ee1bdf47edf87698c1439da4e3da
Giuseppe <glupari100@gmail.com>
2023-10-26 15:56:58 +0100

    005-delete-a-delivery-employee

--- controller/employeeController.ts
+++ controller/employeeController.ts
@@ -35,5 +35,38 @@ module.exports = function(app: Application){
         
     })
 
+    app.get('/delete-delivery-employee', async (req: Request, res: Response) => {
+        let data: Employee[];
+
+        try {
+            data = await employeeService.getemployees()
+            res.render('delete-delivery-employee', { employees: data })      
+            
+            
+        }catch (e) {
+            console.error(e);
+        }      
+        
+    })
+
+    app.post('/delete-delivery-employee/:id', async (req: Request, res: Response) => {
+        
+        let  id = req.params.id
+        console.log("here you go", id);
+             
+        
+        try{
+             await employeeService.deleteDeliveryEmployeeById(id)
+             
+             res.redirect('/delete-delivery-employee')  
+        } catch (e) {
+            console.error(e);
+
+            res.locals.errormessage = e.message
+
+            res.render('delete-delivery-employee')
+        }
+
+    })
     
 }
--- service/employeeService.ts
+++ service/employeeService.ts
@@ -26,3 +26,16 @@ module.exports.getDeliveryEmployeeById = async function (id: number): Promise<Em
         throw new Error('Could not get Employee')
     }    
 }
+
+    module.exports.deleteDeliveryEmployeeById = async function (id: Number): Promise<Employee[]> {
+
+        try {
+            
+            const response = await axios.delete('http://localhost:8080/api/deliveryEmployees/' + id)
+            
+            return response.data
+        } catch (e) {
+            
+            throw new Error('Could not get Employee')
+        }  
+}
